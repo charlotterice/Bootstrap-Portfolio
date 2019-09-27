cdfa162205b1540419400f95e2ed0c64f96f78c6
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-09-26 23:15:00 -0400

    working on firebase

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBob15vJF-aHVOkIJ2eGf_E7kW7pe4mBCo",
+  authDomain: "time-sheet-baa68.firebaseapp.com",
+  databaseURL: "https://time-sheet-baa68.firebaseio.com",
+  projectId: "time-sheet-baa68",
+  storageBucket: "time-sheet-baa68.appspot.com",
+  messagingSenderId: "969988699336",
+  appId: "1:969988699336:web:0c5e86a5e0cf85a827a1fd"
+};
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCVhrDBU4xOTOxIXSgG5smzWlPNqZl374U",
-    authDomain: "portfolio-107e6.firebaseapp.com",
-    databaseURL: "https://portfolio-107e6.firebaseio.com",
-    projectId: "portfolio-107e6",
-    storageBucket: "portfolio-107e6.appspot.com",
-    messagingSenderId: "724122450167"
-  };
-  firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
   
-  var database = firebase.database();
-  
+var db = firebase.database();
   //button to add new contact
   $("#add-contact-button").on("click", function(event) {
     event.preventDefault();
@@ -32,15 +32,27 @@ var config = {
       message: message
     };
     //sends contact data to firebase
-    database.ref().push(newContact);
+    db.ref().push(newContact);
   
     //console log progess
     console.log(newContact.name);
     console.log(newContact.email);
     console.log(newContact.message);
   
-    // clear text boxes
-    $("#contact-name").val("");
-    $("#contact-email").val("");
-    $("#message").val("");
-  });
+    // Alert
+    alert("Message Sent");
+
+ // Clears all of the text-boxes
+ $("#contact-name").val("");
+ $("#contact-email").val("");
+ $("#message").val("");
+});
+
+
+var forEach = function(t, o, r) {
+  if ("[object Object]" === Object.prototype.toString.call(t))
+    for (var c in t)
+      Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+  else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t);
+};
+
